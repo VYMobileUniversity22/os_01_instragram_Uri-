1ce87e38b2740b43c3c05d9a79f772c65782ab56
josep.lopez <josep.lopez@col.vueling.com>
2022-11-07 17:12:57 +0100

    First commit

---
+++ ios_01_instagram_uriUITests/ios_01_instagram_uriUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_01_instagram_uriUITestsLaunchTests.swift
+//  ios_01_instagram_uriUITests
+//
+//  Created by josepL on 7/11/22.
+//
+
+import XCTest
+
+final class ios_01_instagram_uriUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
